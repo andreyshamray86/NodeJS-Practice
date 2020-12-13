7126e94d25aa059000c0538d8d1a302f385f9fa3
Andrey <andreyshamray86@gmail.com>
2020-12-13 22:46:43 +0300

    Creating simple server

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
 
-    res.write('Hello from server');
-    res.end();
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.end(content);
+                }
+
+            );
+        } else if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.end(content);
+                }
+
+            );
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json'
+            });
+
+            const users = [{
+                    name: 'Ivan',
+                    age: 30
+                },
+                {
+                    name: 'Petr',
+                    age: 35
+                }
+            ];
+
+            res.end(JSON.stringify(users));
+        }
+
+
+    } else if (req.method === 'POST') {
+        const body = [];
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        });
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+            res.end(`
+            <h1>Your message: ${message}</h1>
+            `);
+        });
+
+    }
 });
 
 server.listen(3000, () => {
